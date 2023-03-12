8d9e9756d7612cb9765654fabf68f0fc18e4075c
maidesu <vorpeti@gmail.com>
2023-03-12 22:42:35 +0100

    Console now looks like what was planned

--- src/widgets/window.cpp
+++ src/widgets/window.cpp
@@ -1,3 +1,5 @@
+#include <QSplitter>
+
 #include "window.hpp"
 #include "../common/console.hpp"
 
@@ -7,7 +9,15 @@ Window::Window(QWidget *parent) :
     QWidget(parent),
     m_layout(new QVBoxLayout(this))
 {
-    m_layout->addWidget(Console::instance().textEdit());
+    this->setMinimumSize(700, 400);
+
+    QSplitter* splitter = new QSplitter();
+    splitter->addWidget(new QWidget());
+    splitter->addWidget(Console::instance().textEdit());
+    splitter->setOrientation(Qt::Vertical);
+
+    m_layout->addWidget(splitter);
+    //this->setLayout(m_layout);
 }
 
 } // namespace DialogueFromVideo
