10660eaa356012c265df96fc880ad5217e071a66
maidesu <vorpeti@gmail.com>
2024-04-20 18:36:54 +0200

    Add time reverse function

--- include/common/time.hpp
+++ include/common/time.hpp
@@ -13,4 +13,20 @@ QString millisecondsToStringTime(int64_t ms)
     return time.toString("HH:mm:ss.zzz");
 }
 
+int64_t stringTimeToMilliseconds(const QString& string)
+{
+    QTime time = QTime::fromString(string, "HH:mm:ss.zzz");
+
+    if (!time.isValid()) {
+        return -1;
+    }
+
+    int64_t milliseconds = time.hour() * 3600000LL +
+                           time.minute() * 60000LL +
+                           time.second() * 1000LL +
+                           time.msec();
+
+    return milliseconds;
+}
+
 } // namespace DialogueFromVideo
