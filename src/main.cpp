055fa6342d00e6cf9c05e9fdb365936a2940637e
maidesu <vorpeti@gmail.com>
2024-05-21 01:47:21 +0200

    bump ver.: 1.0.4

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv)
 
     QGuiApplication::setApplicationName("Video to Dialogue");
     QGuiApplication::setApplicationDisplayName("V₂D");
-    QGuiApplication::setApplicationVersion("1.0.3");
+    QGuiApplication::setApplicationVersion("1.0.4");
 
     QGuiApplication::setWindowIcon(QIcon(":/icon.ico"));
 
